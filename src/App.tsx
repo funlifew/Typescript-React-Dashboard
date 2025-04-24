5517a40b593b2d964a5942a6830aee163182a21a
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-24 09:56:05 +0330

    🌠 Update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
-import { ThemeProvider } from './contexts/ThemeContext';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import FeaturesSection from './components/FeaturesSection';
-import AboutSection from './components/AboutSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
+import Router from './Router';
 
 /**
- * Main App component that assembles all sections
+ * Main App component that renders the router
  * @returns {React.ReactElement} App component
  */
 const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">
-          <HeroSection />
-          <FeaturesSection />
-          <AboutSection />
-          <ContactSection />
-        </main>
-        <Footer />
-      </div>
-    </ThemeProvider>
-  );
+  return <Router />;
 };
 
 export default App;
